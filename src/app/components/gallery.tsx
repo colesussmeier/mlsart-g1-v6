2dff74a80add7fdd05a128335e8377a92f501092
Cole <colesussmeier@gmail.com>
2024-07-17 21:53:12 -0400

    fix local assets and gallery spacing

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -21,15 +21,15 @@ function Gallery() {
 
     return (
         <>
-            <section className="w-full grid sm:grid-cols-3 grid-cols-1 px-3">
+            <section className="w-full grid sm:grid-cols-3 grid-cols-1 px-4">
                 {landscapeProducts.length > 0 && landscapeProducts.map((product: Product) => {
                     //const [width, height] = product.size.split(' x ').map(size => Number(size.slice(0, -1)));
                     return (
-                        <div key={product.title} className="flex justify-center items-center py-5">
+                        <div key={product.title} className="flex justify-center items-center py-10">
                             {product.isPurchased ? (
                                 <div className="relative">
                                 <Image 
-                                    className="opacity-50 rounded max-h-96 md:max-w-[30vw] md:max-h-[20vw]"
+                                    className="opacity-50 rounded max-h-96 md:max-w-[27vw] md:max-h-[18vw]"
                                     src={product.image} 
                                     alt={product.title} 
                                     width={750}
@@ -44,7 +44,7 @@ function Gallery() {
                                         alt={product.title} 
                                         width={750}
                                         height={550}
-                                        className="rounded md:max-w-[30vw] md:max-h-[20vw]"
+                                        className="rounded md:max-w-[27vw] md:max-h-[18vw]"
                                         />
                                 </Link>
                             )}
@@ -53,14 +53,14 @@ function Gallery() {
                 })}
             </section>
             {floralProducts.length > 0 && <p className="text-2xl lg:text-3xl text-center pt-12 pb-10 lg:pb-16 text-custom-blue">Floral Collection</p>}
-            <section className="w-full grid sm:grid-cols-3 grid-cols-1 px-3">
+            <section className="w-full grid sm:grid-cols-3 grid-cols-1 px-4">
                 {floralProducts.length > 0 && floralProducts.map((product: Product) => {
                     return (
-                        <div key={product.title} className="flex justify-center items-center py-5">
+                        <div key={product.title} className="flex justify-center items-center py-10">
                             {product.isPurchased ? (
                                 <div className="relative">
                                 <Image 
-                                    className="opacity-50 rounded max-h-96 md:max-w-[30vw] md:max-h-[20vw]"
+                                    className="opacity-50 rounded max-h-96 md:max-w-[27vw] md:max-h-[18vw]"
                                     src={product.image} 
                                     alt={product.title} 
                                     width={750}
@@ -75,7 +75,7 @@ function Gallery() {
                                         alt={product.title} 
                                         width={750}
                                         height={550}
-                                        className="rounded md:max-w-[30vw] md:max-h-[20vw]"
+                                        className="rounded md:max-w-[27vw] md:max-h-[18vw]"
                                         />
                                 </Link>
                             )}
