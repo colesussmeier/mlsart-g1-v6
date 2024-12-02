bec51b1f4c4bb28995df4622225230aa869a7c00
Cole <colesussmeier@gmail.com>
2024-12-02 14:21:27 -0500

    nav size update

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between flex-wrap bg-white px-6 py-1 lg:py-6">
-            <p className="text-xl text-custom-blue max-w-44 lg:max-w-xl text-center">Mary Lou Sussmeier <span className="font-medium">Watercolors</span></p>
+            <p className="text-xl text-custom-blue max-w-44 md:text-2xl lg:max-w-xl text-center">Mary Lou Sussmeier <span className="font-medium">Watercolors</span></p>
             <div className="block lg:hidden">
                 <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded border-custom-blue hover:text-gray-500 hover:border-gray-500">
                     <svg className="fill-custom-blue h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v15z"/></svg>
